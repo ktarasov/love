f24706facb2f7909cfd9bce251c6e1af014c20cb
Konstantin Tarasov <ktarasov@peredelka-saitov.ru>
2026-04-09 13:03:45 +0300

    Добавлена генерация для разных архитектур по списку

--- build.zig
+++ build.zig
@@ -1,4 +1,30 @@
 const std = @import("std");
+const TargetQuery = std.Target.Query;
+
+const release_targets = [_]TargetQuery{
+    .{
+        .cpu_arch = .x86_64,
+        .os_tag = .linux,
+    },
+    .{
+        .cpu_arch = .x86_64,
+        .os_tag = .windows,
+    },
+    .{
+        .cpu_arch = .x86_64,
+        .os_tag = .macos,
+    },
+    .{
+        .cpu_arch = .aarch64,
+        .os_tag = .macos,
+    },
+};
+
+const FileExtension = enum {
+    zip,
+    @"tar.xz",
+    @"tar.gz",
+};
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
@@ -31,4 +57,28 @@ pub fn build(b: *std.Build) void {
     const run_exe_tests = b.addRunArtifact(exe_tests);
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&run_exe_tests.step);
+
+    { // zig build release
+        const release_step = b.step("release", "Build release binaries");
+        for (release_targets) |release_target| {
+            const resolved_target = b.resolveTargetQuery(release_target);
+            const exe_release = b.addExecutable(.{
+                .name = "love",
+                .root_module = b.createModule(.{
+                    .root_source_file = b.path("src/main.zig"),
+                    .target = resolved_target,
+                    .optimize = .ReleaseSmall,
+                    .imports = &.{},
+                }),
+            });
+
+            const dir_name = b.fmt("{t}-{t}", .{
+                resolved_target.result.cpu.arch,
+                resolved_target.result.os.tag,
+            });
+            const install_dir = std.Build.InstallDir{ .custom = dir_name };
+            const release_cmd = b.addInstallArtifact(exe_release, .{ .dest_dir = .{ .override = install_dir } });
+            release_step.dependOn(&release_cmd.step);
+        }
+    }
 }
